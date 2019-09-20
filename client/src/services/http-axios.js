736e0c9c7984ecea23a1e3da2d5a69a5ded6359e
Robert Rock <rdrock7147@gmail.com>
2019-09-20 17:05:33 -0400

    Http Service
    Added methods for fetching a single product by id or multiple products.

--- client/src/services/http-axios.js
+++ client/src/services/http-axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const orderUrl = 'https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json'
+const productUrl = 'https://prodcat.gopuff.com/api/products'
 
 const getOrder = async () => {
   try {
@@ -11,8 +12,50 @@ const getOrder = async () => {
   }
 }
 
+const fetchProductById = async (productId) => {
+  try {
+    const response = await axios.get(productUrl, {
+      params: {
+        location_id: '-1', // -1 for testing
+        product_id: productId
+      }
+    })
+
+    if (response.status === 200) {
+      return response.data
+    } else {
+      return response
+    }
+  } catch (err) {
+    console.log(`Error fetching product id: ${productId}`)
+  }
+}
+
+const fetchMultipleProductsById = async (productIds) => {
+  try {
+    // TODO: Check 'productIds' to ensure they are proper format
+
+    const response = await axios.get(productUrl, {
+      params: {
+        location_id: '-1', // -1 for testing
+        product_ids: productIds
+      }
+    })
+
+    if (response.status === 200) {
+      return response.data
+    } else {
+      return response
+    }
+  } catch (err) {
+    console.log(`Error fetching product id: ${productId}`)
+  }
+}
+
 const httpService = {
-  getOrder
+  getOrder,
+  fetchProductById,
+  fetchMultipleProductsById
 }
 
 export default httpService
