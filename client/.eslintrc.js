a57fdcdbf7e1ddb4cfce8ca1186efa19faed919e
Robert Rock <rdrock7147@gmail.com>
2019-09-20 16:55:39 -0400

    Cart
    Changed ESlint away from Standard. Skeleton starter for cart service.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,27 +1,56 @@
+// module.exports = {
+//   root: true,
+//   env: {
+//     node: true
+//   },
+//   'extends': [
+//     'plugin:vue/essential',
+//     '@vue/standard'
+//   ],
+//   rules: {
+//     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+//     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+//   },
+//   parserOptions: {
+//     parser: 'babel-eslint'
+//   },
+//   overrides: [
+//     {
+//       files: [
+//         '**/__tests__/*.{j,t}s?(x)'
+//       ],
+//       env: {
+//         jest: true
+//       }
+//     }
+//   ]
+// }
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    codeFrame: false
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  env: {
+    node: true,
+    es6: true,
+    mocha: false
+  },
+  rules: {
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'import/extensions': ['off', 'never'],
+    'import/newline-after-import': 'off',
+    'indent': 'off',
+    'no-use-before-define': [
+      'error',
+      'nofunc'
+    ],
+    'quotes': ['error', 'single'],
+    'semi': 'off'
+  }
 }
---
+++ client/src/services/cart.js
@@ -0,0 +1,27 @@
+/**
+ * Sum 'price' property for each product, multiplying by 'quantity'
+ */
+function calculateSubtotal (products) {
+
+}
+
+function calculateCreditCoupon (products) {
+
+}
+
+/**
+ * Updates specified product total number in cart
+ *
+ * @param {*} product
+ * @param {number} newQuantity
+ */
+function updateProductQuantity (product, newQuantity) {
+}
+
+const cartService = {
+  calculateSubtotal,
+  calculateCreditCoupon,
+  updateProductQuantity
+}
+
+export default cartService
